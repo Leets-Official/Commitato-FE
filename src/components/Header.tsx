07622fd78ba6c5d94caf445fd7473e6237ca672c
김지원 <jiwonkim0531@gachon.ac.kr>
2025-02-13 15:07:28 +0900

    feat: Header UI 구현

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,56 @@
-const Header = () => {
-  return <>Header</>;
+import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+const Header: React.FC = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(() => {
+    return localStorage.getItem('accessToken') === 'true';
+  });
+
+  useEffect(() => {
+    localStorage.setItem('accessToken', String(isLoggedIn));
+  }, [isLoggedIn]);
+
+  return (
+    <header className="w-full h-[63px] bg-white">
+      <div className="flex items-center justify-between py-4 px-6">
+        <div className="flex items-center space-x-8">
+          <Link to="/" className="font-staatliches text-body">
+            COMMITATO
+          </Link>
+          <div className="space-x-8">
+            <Link to="/" className="text-grey text-assistive hover:text-black">
+              HOME
+            </Link>
+
+            {isLoggedIn && (
+              <Link
+                to="/mypage"
+                className="text-grey text-assistive hover:text-black"
+              >
+                MY PAGE
+              </Link>
+            )}
+
+            <Link
+              to="/ranking"
+              className="text-grey text-assistive hover:text-black"
+            >
+              RANKING
+            </Link>
+          </div>
+        </div>
+
+        {isLoggedIn && (
+          <button
+            onClick={() => setIsLoggedIn(false)}
+            className="text-grey hover:text-black"
+          >
+            Logout
+          </button>
+        )}
+      </div>
+    </header>
+  );
 };
 
 export default Header;
