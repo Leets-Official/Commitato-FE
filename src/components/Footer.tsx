b4bf1113a3df25e8832ebeef896fd5cf908424b9
김지원 <jiwonkim0531@gachon.ac.kr>
2025-02-16 18:46:43 +0900

    fix: mainPage 구별 css 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,25 +1,39 @@
 import React from 'react';
 import Github from '@/assets/icon/footerGithub.svg?react';
 
-const Footer: React.FC = () => {
+interface FooterProps {
+  isMainPage?: boolean;
+}
+
+const Footer: React.FC<FooterProps> = ({ isMainPage = false }) => {
   return (
-    <footer className="w-full h-[236px] flex text-white py-7">
-      <div className="justify-between items-start px-9">
-        <div className="flex items-center space-x-2">
+    <footer
+      className={`w-full ${
+        isMainPage ? 'h-[236px]' : 'h-[143px]'
+      } flex text-white text-center py-7 items-center justify-center`}
+    >
+      <div className="px-9">
+        <div className="flex justify-center items-center space-x-2">
           <span className="font-staatliches text-body">COMMITATO</span>
           <Github />
         </div>
 
         <div className="flex-col text-button text-grey mt-[5%] space-y-3">
-          <p>
-            대표 | 가천대학교 Leets 동아리 내 commitato 팀 &nbsp; &nbsp; &nbsp;
-            &nbsp;사이트명 | Commitato
-          </p>
-          <p>
-            주소 | 가천대학교 글로벌캠퍼스 : (13120) 경기도 성남시 수정구
-            성남대로 1342
-          </p>
-          <p>본 사이트에 게재된 개인 정보 및 작업물의 무단 도용을 금합니다.</p>
+          {isMainPage && (
+            <div className="flex-col text-button text-grey mt-[5%] space-y-3">
+              <p>
+                대표 | 가천대학교 Leets 동아리 내 commitato 팀 &nbsp; &nbsp;
+                사이트명 | Commitato
+              </p>
+              <p>
+                주소 | 가천대학교 글로벌캠퍼스 : (13120) 경기도 성남시 수정구
+                성남대로 1342
+              </p>
+              <p>
+                본 사이트에 게재된 개인 정보 및 작업물의 무단 도용을 금합니다.
+              </p>
+            </div>
+          )}
           <p>Copyright 2024. Commitato all rights reserved.</p>
         </div>
       </div>
