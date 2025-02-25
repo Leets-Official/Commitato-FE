11855d39e7795de27dd1b9236556cf276761c377
dahhyeon <dahhyeon22@gmail.com>
2025-02-25 16:52:49 +0900

    feat:#67 메인 텍스트 애니메이션 추가

--- src/components/main/Section.tsx
+++ src/components/main/Section.tsx
@@ -1,4 +1,6 @@
 import GithubLoginButton from '@/components/main/GithubLogin';
+import BannerIcon from '@/assets/icon/ic_main_banner.svg?react';
+import { TextFade } from './TextFade';
 
 interface SectionProps {
   className?: string;
@@ -6,12 +8,18 @@ interface SectionProps {
 
 const Section: React.FC<SectionProps> = ({ className }) => {
   return (
-    <div
-      className={`font-staatliches text-[13rem] flex flex-col justify-start  items-center ${className}`}
-    >
-      <div>COMMITATO</div>
-      <GithubLoginButton />
-    </div>
+    <>
+      <div className={`flex flex-col justify-start  items-center ${className}`}>
+        <TextFade direction={'up'} duration={1.9} delay={0.1}>
+          <div className="font-staatliches text-[13rem]">COMMITATO</div>
+        </TextFade>
+        <GithubLoginButton />
+      </div>
+      <div className="flex items-center gap-7">
+        <BannerIcon className="w-[160px]" />
+        <span className="font-staatliches text-[96px]">ABOUT COMMITATO</span>
+      </div>
+    </>
   );
 };
 
