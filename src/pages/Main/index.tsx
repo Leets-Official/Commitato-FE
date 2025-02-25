0f304310aff8d1d1db27067af46c64a74f716bed
김지원 <jiwonkim0531@gachon.ac.kr>
2025-02-25 15:18:19 +0900

    feat: 로그인 성공시 main 모달 뜨도록 구현

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,14 +1,35 @@
 import Footer from '@/components/Footer';
 import Header from '@/components/Header';
 import GithubLoginButton from '@/components/main/GithubLogin';
+import WelcomeModal from '@/components/modal/WelcomeModal';
+import { useEffect, useState } from 'react';
 
 const MainPage = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [githubId, setGithubId] = useState<string | null>(null);
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem('accessToken');
+    const storedGithubId = localStorage.getItem('githubId');
+
+    if (accessToken && storedGithubId) {
+      setGithubId(storedGithubId);
+      setIsModalOpen(true);
+    }
+  }, []);
+
   return (
     <>
       <Header />
       <GithubLoginButton />
       main
       <Footer isMainPage />
+      {isModalOpen && (
+        <WelcomeModal
+          onClose={() => setIsModalOpen(false)}
+          githubId={githubId}
+        />
+      )}
     </>
   );
 };
