9e1db5828cf59ef85bdaafa10e27a7928fc30bbd
dahhyeon <dahhyeon22@gmail.com>
2025-03-21 16:17:23 +0900

    fix:#76 랭킹 조회 오류 해결

--- src/apis/ranking/ranking.api.ts
+++ src/apis/ranking/ranking.api.ts
@@ -2,16 +2,16 @@ import api from '@/apis/api';
 
 const PATH = '/user';
 
-const getRankingApi = async () => {
+export const getRankingApi = async (page: number = 0, size: number = 10) => {
   try {
-    const res = await api.get(`${PATH}/ranking`);
-    return res.data.result;
+    const res = await api.get(`${PATH}/ranking`, { params: { page, size } });
+    return res.data.result.content;
   } catch (error) {
     error instanceof Error ? error.message : '랭킹 조회 오류가 발생했습니다: ';
   }
 };
 
-const getUserIdApi = async (githubId: string) => {
+export const getUserIdApi = async (githubId: string) => {
   try {
     const res = await api.get(`${PATH}/search/${githubId}`);
     return res.data.result;
@@ -21,4 +21,3 @@ const getUserIdApi = async (githubId: string) => {
       : '유저 아이디 조회 오류가 발생했습니다: ';
   }
 };
-export default { getRankingApi, getUserIdApi };
--- src/components/Ranking/RankingList.tsx
+++ src/components/Ranking/RankingList.tsx
@@ -1,109 +1,140 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import RankingItem from '@/components/Ranking/RankingItem';
 import Line from '@/assets/icon/myPageLine.svg?react';
+import { getRankingApi } from '@/apis/ranking/ranking.api';
 
-const rankingData = [
-  {
-    rank: 1,
-    userId: 1,
-    user: 'adflakjd',
-    tier: 'CEO 감자',
-    commitDay: '7일',
-    exp: 120,
-    change: 'up',
-  },
-  {
-    rank: 2,
-    userId: 2,
-    user: 'dalzzy',
-    tier: 'CEO 감자',
-    commitDay: '5일',
-    exp: 110,
-    change: 'down',
-    isMe: true,
-  },
-  {
-    rank: 3,
-    userId: 3,
-    user: 'woneeeee',
-    tier: 'CEO 감자',
-    commitDay: '4일',
-    exp: 100,
-    change: 'none',
-  },
-  {
-    rank: 4,
-    userId: 4,
-    user: 'githubID_4',
-    tier: '개발자 감자',
-    commitDay: '4일',
-    exp: 90,
-    change: 'none',
-  },
-  {
-    rank: 5,
-    userId: 5,
-    user: 'kimmm',
-    tier: '말하는 감자',
-    commitDay: '4일',
-    exp: 80,
-    change: 'none',
-  },
-  {
-    rank: 6,
-    userId: 6,
-    user: 'githubID_6',
-    tier: '개발자 감자',
-    commitDay: '4일',
-    exp: 80,
-    change: 'none',
-  },
-  {
-    rank: 7,
-    userId: 7,
-    user: 'githubID_7',
-    tier: '바보 감자',
-    commitDay: '4일',
-    exp: 100,
-    change: 'none',
-  },
-  {
-    rank: 8,
-    userId: 8,
-    user: 'githubID_8',
-    tier: '개발자 감자',
-    commitDay: '4일',
-    exp: 80,
-    change: 'none',
-  },
-  {
-    rank: 9,
-    userId: 9,
-    user: 'githubID_9',
-    tier: '바보 감자',
-    commitDay: '4일',
-    exp: 80,
-    change: 'none',
-  },
-  {
-    rank: 10,
-    userId: 10,
-    user: 'githubID_10',
-    tier: '개발자 감자',
-    commitDay: '4일',
-    exp: 80,
-    change: 'none',
-  },
-];
+// const rankingData = [
+//   {
+//     rank: 1,
+//     userId: 1,
+//     user: 'adflakjd',
+//     tier: 'CEO 감자',
+//     commitDay: '7일',
+//     exp: 120,
+//     change: 'up',
+//   },
+//   {
+//     rank: 2,
+//     userId: 2,
+//     user: 'dalzzy',
+//     tier: 'CEO 감자',
+//     commitDay: '5일',
+//     exp: 110,
+//     change: 'down',
+//     isMe: true,
+//   },
+//   {
+//     rank: 3,
+//     userId: 3,
+//     user: 'woneeeee',
+//     tier: 'CEO 감자',
+//     commitDay: '4일',
+//     exp: 100,
+//     change: 'none',
+//   },
+//   {
+//     rank: 4,
+//     userId: 4,
+//     user: 'githubID_4',
+//     tier: '개발자 감자',
+//     commitDay: '4일',
+//     exp: 90,
+//     change: 'none',
+//   },
+//   {
+//     rank: 5,
+//     userId: 5,
+//     user: 'kimmm',
+//     tier: '말하는 감자',
+//     commitDay: '4일',
+//     exp: 80,
+//     change: 'none',
+//   },
+//   {
+//     rank: 6,
+//     userId: 6,
+//     user: 'githubID_6',
+//     tier: '개발자 감자',
+//     commitDay: '4일',
+//     exp: 80,
+//     change: 'none',
+//   },
+//   {
+//     rank: 7,
+//     userId: 7,
+//     user: 'githubID_7',
+//     tier: '바보 감자',
+//     commitDay: '4일',
+//     exp: 100,
+//     change: 'none',
+//   },
+//   {
+//     rank: 8,
+//     userId: 8,
+//     user: 'githubID_8',
+//     tier: '개발자 감자',
+//     commitDay: '4일',
+//     exp: 80,
+//     change: 'none',
+//   },
+//   {
+//     rank: 9,
+//     userId: 9,
+//     user: 'githubID_9',
+//     tier: '바보 감자',
+//     commitDay: '4일',
+//     exp: 80,
+//     change: 'none',
+//   },
+//   {
+//     rank: 10,
+//     userId: 10,
+//     user: 'githubID_10',
+//     tier: '개발자 감자',
+//     commitDay: '4일',
+//     exp: 80,
+//     change: 'none',
+//   },
+// ];
 
 interface RankingListProps {
   searchId: string;
 }
 const RankingList: React.FC<RankingListProps> = ({ searchId }) => {
-  const filteredData = rankingData.filter(item =>
-    item.user.toLowerCase().includes(searchId.toLowerCase()),
-  );
-  const myRanking = rankingData.find(item => item.isMe);
+  const [rankingData, setRankingData] = useState<any[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchRanking = async () => {
+      try {
+        setIsLoading(true);
+        const data = await getRankingApi();
+        setRankingData(data);
+      } catch (error) {
+        setError('랭킹 데이터를 불러오는 중 오류가 발생했습니다.');
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchRanking();
+  }, []);
+
+  const filteredData =
+    rankingData.filter(item =>
+      item.githubId.toLowerCase().includes(searchId.toLowerCase()),
+    ) || [];
+
+  const myRanking = rankingData.find(item => item.isMe) || null;
+
+  if (isLoading) {
+    return <p className="text-center text-grey font-Bold">로딩 중...</p>;
+  }
+
+  if (error) {
+    return <p className="text-center text-red-500 font-Bold">{error}</p>;
+  }
 
   return (
     <div className="w-full">
