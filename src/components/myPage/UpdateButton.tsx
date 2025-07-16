0ab6756808ba5f0697748e5460754f8268039208
김지원 <jiwonkim0531@gachon.ac.kr>
2025-07-16 18:13:16 +0900

    feat: 현재 시간에 따른 업데이트 버튼 중단

--- src/components/myPage/UpdateButton.tsx
+++ src/components/myPage/UpdateButton.tsx
@@ -1,13 +1,30 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import updateButton from '@/apis/myPage/updateButton.api';
 import { UserTypes } from 'commitato-types';
+import getUserCommits from '@/apis/myPage/userCommit.api';
 
 interface UpdateButtonProps {
   onUpdated: (updatedUser: UserTypes) => void;
 }
 
+const isInRestrictedTime = () => {
+  const now = new Date();
+  const hour = now.getHours();
+  return hour === 6;
+};
+
 const UpdateButton = ({ onUpdated }: UpdateButtonProps) => {
   const [isLoading, setIsLoading] = useState(false);
+  const [isRestricted, setIsRestricted] = useState(isInRestrictedTime());
+  const githubId = localStorage.getItem('githubId');
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIsRestricted(isInRestrictedTime());
+    }, 60 * 1000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   const handleClick = async () => {
     try {
@@ -16,6 +33,12 @@ const UpdateButton = ({ onUpdated }: UpdateButtonProps) => {
       console.log(updatedUser);
       if (updatedUser) {
         onUpdated(updatedUser);
+        if (githubId) {
+          const updatedCommits = await getUserCommits(githubId);
+          console.log(updatedCommits);
+        } else {
+          console.log('githubId가 없습니다.');
+        }
       }
     } catch (err) {
       console.error('업데이트 실패:', err);
@@ -35,7 +58,7 @@ const UpdateButton = ({ onUpdated }: UpdateButtonProps) => {
           <div className="w-5 h-5 border-2 border-white border-t-transparent rounded-full animate-spin" />
         ) : (
           <p className="font-Regular text-body font-staatliches text-white">
-            UPDATE
+            {isRestricted ? '잠시 중단됨' : 'UPDATE'}
           </p>
         )}
       </button>
